d33a985ac6b4ae7162001b8bbfc317983f119d6e
tognoek <zgye28102003@gmail.com>
2023-08-22 22:27:47 +0700

    Tim so lon nhat -- C++

--- main.cpp
+++ main.cpp
@@ -25,49 +25,7 @@ void swap(long long &xs, long long &ys)
 
 int main()
 {
-    int q, n;
-    long long a, h, x, y, s, u, v;
-    cin >> q;
-    while (q--)
-    {
-        /* code */
-        cin >> n >> h;
-        cin >> x;
-        cin >> y;
-        if (x < y)
-        {
-            swap(x, y);
-        }
-        for (int i = 3; i <= n; i++)
-        {
-            cin >> a;
-            if (a >= x)
-            {
-                y = x;
-                x = a;
-            }
-            else
-            {
-                if (a > y)
-                {
-                    y = a;
-                }
-            }
-        }
-        s = x + y;
-        u = h - (int)(h / s) * s;
-        if (x >= u && u > 0)
-        {
-            v = (h / s) * 2 + 1;
-        }
-        if (x < u && u > 0)
-        {
-            v = (h / s) * 2 + 2;
-        }
-        if (u <= 0)
-        {
-            v = (h / s) * 2;
-        }
-        cout << v << endl;
-    }
+    int a, b;
+    cin >> a >> b;
+    cout << max(a, b);
 }
