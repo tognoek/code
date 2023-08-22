bd41a6509cc31ee53b026461556deb9aa6b8f69d
tognoek <zgye28102003@gmail.com>
2023-08-22 23:18:26 +0700

    Cong, tru, nhan, chia --Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/vl08)
+# [Link De Bai](https://luyencode.net/problem/cb04)
--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 a, b = map(int, input().split())
-a = int(a / 2)
-b = int(b / 2)
-
-s = 0
-s = sum(i * 2 for i in range(a, b + 1))
-print(s)
+print(a + b)
+print(a - b)
+print(a * b)
+if b == 0:
+    print("ERROR")
+else:
+    print("{:.2f}".format(a / b))
