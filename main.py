c0ee18ee98cce445d218c5c1249583b2f65472c6
tognoek <zgye28102003@gmail.com>
2023-08-22 22:44:22 +0700

    Tinh tong phien ban 1 -- Python

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 n = int(input())
 
-s = (n + 2) * int((n - 2 + 1) / 2) + int((n + 2) / 2) * (int(n - 2 + 1) % 2) + n * 2
+s = (n + 1) * int((n - 1 + 1) / 2) + int((n + 1) / 2) * (int(n - 1 + 1) % 2)
 print(s)
