6723b5297bb3d5110a0e42bb28f48dd77c0da290
tognoek <zgye28102003@gmail.com>
2023-08-22 22:42:55 +0700

    Tong s (2 + 3 + .. n) + 2*n -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/vl01)
+# [Link De Bai](https://luyencode.net/problem/vl03)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
-a, b= map(int, input().split())
-for i in range(a, b + 1):
-    print(i, end = " ")
+n = int(input())
+
+s = (n + 2) * int((n - 2 + 1) / 2) + int((n + 2) / 2) * (int(n - 2 + 1) % 2) + n * 2
+print(s)
