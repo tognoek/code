c1a3a701fdd22554510d736082aeef4b9d541f61
tognoek <zgye28102003@gmail.com>
2023-08-25 22:33:01 +0700

    chi la them mot be java vao thoi

---
+++ main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class main {
+    public static void __main__(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+
+
+        System.out.println("Hello, World!");
+
+
+        sc.close();
+    }
+}
--- test.py
+++
@@ -1,5 +0,0 @@
-a, b = map(int, input().split())
-
-s = 0
-s = sum(i * 2 for i in range(a, b + 1))
-print(s)
