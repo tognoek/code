3aee24292b627885d4866a4bb89f8b8c6cbc807d
tognoek <zgye28102003@gmail.com>
2023-08-26 09:24:08 +0700

    Liet ke uoc duong -- Python

--- tognoek.py
+++ tognoek.py
@@ -1,2 +1,7 @@
-a = 9.2424
-print("{:.2f}".format(a))
+n = int(input())
+if n == 0:
+    print("INF")
+else:
+    for i in range(abs(n), 0, -1):
+        if n % i == 0:
+            print(i, end = " ")
