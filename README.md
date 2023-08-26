5b52da4974f57b6e49da5736cbc502eac473f243
tognoek <zgye28102003@gmail.com>
2023-08-26 09:28:29 +0700

    tong binh phuong -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/vl13)
+# [Link De Bai](https://luyencode.net/problem/sum2)
--- tognoek.py
+++ tognoek.py
@@ -1,7 +1,3 @@
 n = int(input())
-if n == 0:
-    print("INF")
-else:
-    for i in range(abs(n), 0, -1):
-        if n % i == 0:
-            print(i, end = " ")
+s = (n * (n + 1) / 2) * (2 * n + 1) / 3
+print(int(s))
