dd802b22f5371eeb72d9acdf2d9501685972ccc2
tognoek <zgye28102003@gmail.com>
2023-08-27 08:26:18 +0700

    Tim so dao nguoc -- Python

--- tognoek.py
+++ tognoek.py
@@ -1,10 +1,3 @@
-s = 0
-a = [0]
-for i in range(1, 1000009):
-    s += i
-    a.append(a[i-1] + 1 / s)
-
-t = int(input())
-for i in range(t):
-    x = int(input())
-    print("{:.8f}".format(a[x]))
+a = input()
+b = a[::-1]
+print(int(b))
