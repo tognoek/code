1efc7466497c2aebfbb26bf9cf2cafaf5a39545f
tognoek <zgye28102003@gmail.com>
2023-08-27 10:18:35 +0700

    Phep toan lop 3 -- Java

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/primfac)
+# [Link De Bai](https://luyencode.net/problem/phtinh)
--- tognoek.java
+++ tognoek.java
@@ -14,13 +14,11 @@ public class tognoek {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int n, numbermax, numbermemory;
-        n = Integer.parseInt(sc.nextLine());
-        numbermax = sc.nextInt();
-        for (int i = 2; i <= n; i++){
-            numbermemory = sc.nextInt();
-            numbermax = Math.max(numbermemory, numbermax);
-        }
-        System.out.println(numbermax);
+        int a, b, c;
+        a = sc.nextInt();
+        b = sc.nextInt();
+        c = sc.nextInt();
+        System.out.println((a - b ) * c);
+        sc.close();
     }
 }
