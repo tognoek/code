d955d900c023cf8bc5bda2570abfd1434e2500dd
tognoek <zgye28102003@gmail.com>
2023-08-27 10:24:47 +0700

    So gap doi -- Java

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/phtinh)
+# [Link De Bai](https://luyencode.net/problem/gapdoi)
--- tognoek.java
+++ tognoek.java
@@ -14,11 +14,9 @@ public class tognoek {
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        int a, b, c;
+        int a;
         a = sc.nextInt();
-        b = sc.nextInt();
-        c = sc.nextInt();
-        System.out.println((a - b ) * c);
+        System.out.println(a + a);
         sc.close();
     }
 }
