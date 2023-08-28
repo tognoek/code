f88b82ecf8ce9951b6d38a8c2d513a8ab0ee8088
tognoek <zgye28102003@gmail.com>
2023-08-28 10:25:34 +0000

    Chuan hoa xua -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/gapdoi)
+# [Link De Bai](https://luyencode.net/problem/str02)
--- tognoek.py
+++ tognoek.py
@@ -1,15 +1,3 @@
-n, k = map(int, input().split())
-
-if k == 0:
-    for i in range(n):
-        print(i + 1, end = " ")
-else:
-    if n % (2 * k) != 0:
-        print(-1)
-    else:
-        l = int(n / (2 * k))
-        for t in range(1, l + 1):
-            for i in range((t - 1) * (2 * k) , (t - 1) * (2 * k) + k):
-                print(i + 1 + k, end = " ")
-            for i in range((t - 1) * (2 * k) + k, t * (2 * k)):
-                print(i - k + 1, end = " ")
+n = int(input())
+for _ in range(n):
+    print(" ".join(input().split()).lower().title())
