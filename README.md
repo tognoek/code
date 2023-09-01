a2f6b3b52229829f6ca574035d843be7d3d6c2fe
tognoek <zgye28102003@gmail.com>
2023-09-01 12:57:27 +0000

    Tim so chinh phuong trong ma tran -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/mt05)
+# [Link De Bai](https://luyencode.net/problem/mt06)
--- tognoek.py
+++ tognoek.py
@@ -1,23 +1,16 @@
-a, b, n = map(int, input().split())
-x = list(map(int, input().split()))
-k = 0
+import math
+a, b = map(int, input().split())
 c = []
-n = n - 1
-for i in range(a * b):
-    if (i - n) % b == 0:
-        c.append(x[i])
-
-c.sort()
-
-for i in range(a * b):
-    if (i - n) % b == 0:
-        x[i] = c[k]
-        k += 1
-
-k = 0
-
 for i in range(a):
+    d = list(map(int, input().split()))
     for t in range(b):
-        print(x[k], end = " ")
-        k += 1
-    print()
+        if d[t] >= 0 and int(math.pow(int(math.sqrt(d[t])), 2)) == d[t]:
+            c.append(d[t])
+c.sort()
+if len(c) > 0:
+    print(c[0], end = " ")
+else:
+    print("NOT FOUND")
+for i in range(1, len(c)):
+    if c[i] > c[i-1]:
+        print(c[i], end = " ")
