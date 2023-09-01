40178811f5307e09959203e1efce3e537430b903
tognoek <zgye28102003@gmail.com>
2023-09-01 12:50:15 +0000

    Sap xep ma tran 2 -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/mt04)
+# [Link De Bai](https://luyencode.net/problem/mt05)
--- tognoek.py
+++ tognoek.py
@@ -1,19 +1,23 @@
 a, b, n = map(int, input().split())
 x = list(map(int, input().split()))
 k = 0
-n = (n - 1) * b
-for i in range(a):
-    if k != n:
-        for t in range(b):
-            print(x[k], end = " ")
-            k += 1
-    else:
-        c = []
-        for t in range(b):
-            c.append(x[k])
-            k += 1
-        c.sort()
-        for t in range(b):
-            print(c[t], end = " ")
+c = []
+n = n - 1
+for i in range(a * b):
+    if (i - n) % b == 0:
+        c.append(x[i])
 
+c.sort()
+
+for i in range(a * b):
+    if (i - n) % b == 0:
+        x[i] = c[k]
+        k += 1
+
+k = 0
+
+for i in range(a):
+    for t in range(b):
+        print(x[k], end = " ")
+        k += 1
     print()
