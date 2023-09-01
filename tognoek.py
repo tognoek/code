e9499b500c5b01cd61077769fb62794bf1623d25
tognoek <zgye28102003@gmail.com>
2023-09-01 12:09:15 +0000

    In ra so chia het 3 -- Python

--- tognoek.py
+++ tognoek.py
@@ -1,3 +1,8 @@
-import math
 a, b = map(int, input().split())
-print(math.lcm(a, b))
+k = 0
+for i in range(b-1, a, -1):
+    if i % 3 == 0:
+        k = 1
+        print(i, end = " ")
+if k == 0:
+    print("NOT FOUND")
