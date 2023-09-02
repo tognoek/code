81fb5847885f7071e35cbd6f7ae0c3550b068e25
tognoek <zgye28102003@gmail.com>
2023-09-02 13:15:52 +0000

    Nhan so lon -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 Chỉ có Link
-# [Link De Bai](https://luyencode.net/problem/perfect)
+# [Link De Bai](https://luyencode.net/problem/mulbig)
--- tognoek.py
+++ tognoek.py
@@ -1,7 +1,3 @@
-n = int(input())
-a = list(map(int, input().split()))
-a.sort()
-m = a[1] - a[0]
-for i in range(1, n):
-    m = min(a[i] - a[i-1], m)
-print(m)
+a = int(input())
+b = int(input())
+print(a * b)
