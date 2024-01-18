6007b2eb59f5a7fdcef4b6ca01a0f0a201b4327b
tognoek <zgye28102003@gmail.com>
2024-01-18 11:55:22 +0700

    Nhung la co vui ve -- Python

--- tognoek.py
+++ tognoek.py
@@ -1,3 +1,8 @@
 import math
-a, k = map(int, input().split())
-print(int(a / k), a % k)
+# a = map(int, input().split())
+# a = list(map(int, input().split()))
+a = int(input())
+do = int(a / 3)
+xanh = a - do
+print(do)
+print(xanh)
