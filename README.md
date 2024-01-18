161c493b68416367015d0b1bd08cc97848bd1e2c
tognoek <zgye28102003@gmail.com>
2024-01-18 17:18:15 +0700

    Cap so cong -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 togneok xin chào các bạn !!!
-- [Đề bài](https://c.tmath.vn/problem/a02c000001)
+- [Đề bài](https://c.tmath.vn/problem/a02d000001)
--- tognoek.py
+++ tognoek.py
@@ -1,13 +1,10 @@
 import math
-# a = map(int, input().split())
+a, b, c = map(int, input().split())
 # a = list(map(int, input().split()))
-a = int(input())
-if a < 101:
-    res = a * 2000
-elif a < 201:
-    res = 100 * 2000 + (a - 100) *  3000
-elif a < 301:
-    res = 100 * 2000 + 100 * 3000 + (a - 200) * 5000
+# a = int(input())
+if (c - a) % b == 0:
+    res = int((c - a) / b) + 1
 else:
-    res = 100 * 2000 + 100 * 3000 + 100 * 5000 + (a - 300) *  10000
+    res = -1
+
 print(res)
