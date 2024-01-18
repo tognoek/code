1f68f2309f6d897dd4fb40ef67003e84d6a59145
tognoek <zgye28102003@gmail.com>
2024-01-18 17:28:46 +0700

    In so tu nhien -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 togneok xin chào các bạn !!!
-- [Đề bài](https://c.tmath.vn/problem/a03a000004)
+- [Đề bài](https://c.tmath.vn/problem/a03a000005)
--- tognoek.py
+++ tognoek.py
@@ -1,6 +1,7 @@
 import math
 # a, b, c = map(int, input().split())
 # a = list(map(int, input().split()))
-# a = int(input())
-s, n = map(str, input().split())
-print(s * int(n))
+a = int(input())
+# s, n = map(str, input().split())
+for i in range(1, a + 1, 1):
+    print(i, end = " ")
