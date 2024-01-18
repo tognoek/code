0e88b09d3996d889ba44aae3c456d2469dc32379
tognoek <zgye28102003@gmail.com>
2024-01-18 17:27:10 +0700

    Chu so lap -- Python

--- tognoek.py
+++ tognoek.py
@@ -1,10 +1,6 @@
 import math
-a, b, c = map(int, input().split())
+# a, b, c = map(int, input().split())
 # a = list(map(int, input().split()))
 # a = int(input())
-if (c - a) % b == 0:
-    res = int((c - a) / b) + 1
-else:
-    res = -1
-
-print(res)
+s, n = map(str, input().split())
+print(s * int(n))
