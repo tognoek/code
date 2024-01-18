0cf7ff626e79bdab571bcc5515e25113d70d22c4
tognoek <zgye28102003@gmail.com>
2024-01-18 17:14:27 +0700

    Hoa don tien dien -- Pyhton

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 togneok xin chào các bạn !!!
-- [Đề bài](https://c.tmath.vn/problem/a02b000008)
+- [Đề bài](https://c.tmath.vn/problem/a02c000001)
--- tognoek.py
+++ tognoek.py
@@ -1,9 +1,13 @@
 import math
 # a = map(int, input().split())
 # a = list(map(int, input().split()))
-# a = int(input())
-s = input()
-if int(s[0]) + int(s[1]) == int(s[2]) + int(s[3]):
-    print("YES")
+a = int(input())
+if a < 101:
+    res = a * 2000
+elif a < 201:
+    res = 100 * 2000 + (a - 100) *  3000
+elif a < 301:
+    res = 100 * 2000 + 100 * 3000 + (a - 200) * 5000
 else:
-    print("NO")
+    res = 100 * 2000 + 100 * 3000 + 100 * 5000 + (a - 300) *  10000
+print(res)
