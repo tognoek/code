ab7eb16d992b4e60da51caa859b677aeb1669d66
tognoek <zgye28102003@gmail.com>
2024-01-19 09:09:04 +0700

    Ban co vua -- Python

--- README.md
+++ README.md
@@ -1,2 +1,2 @@
 togneok xin chào các bạn !!!
-- [Đề bài](https://c.tmath.vn/problem/a03b000002)
+- [Đề bài](https://c.tmath.vn/problem/a03c000001)
--- tognoek.py
+++ tognoek.py
@@ -1,7 +1,13 @@
 import math
-a, b = map(int, input().split())
+# a, b = map(int, input().split())
 # a = list(map(int, input().split()))
-# a = int(input())
+n = int(input())
 # s, n = map(str, input().split())
-for _ in range(a):
-    print('#' * b)
+s = ['W', 'B']
+for i in range(n):
+    k = i % 2
+    for t in range(n):
+        print(s[k], end = '')
+        k = k + 1
+        k = k % 2
+    print()
