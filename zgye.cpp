a44aeb9019cb01dd4736f41b4471f236e989eddb
tognoek <zgye28102003@gmail.com>
2024-01-23 20:22:20 +0700

    De thi Bach Khao Da Nang Test -- C: Vi tri mau -- C++

--- zgye.cpp
+++ zgye.cpp
@@ -2,14 +2,53 @@
 
 using namespace std;
 
-int main(){
+int a[5];
 
-    int a, b, c, d;
-    int s, m;
-    cin>>a>>b>>c>>d;
-    s = a + b + c + d;
-    m = s / 60;
-    s = s - (m * 60);
-    cout<<m<<endl<<s;
-    return 0;
+long long trand(long long r, long long e){
+    long long tmp;
+    tmp = e / 2;
+    if (r > tmp){
+        return e - r + 1;
+    }
+    else{
+        return r;
+    }
+}
+long long min(long long a, long long b){
+    return a > b ? b : a;
+}
+
+int solve(long long u, long long v, long long m){
+    u = trand(u, m);
+    v = trand(v, m);
+    long long tg;
+    if (m % 2 == 1){
+        if (u == m % 2 + 1){
+            return v % 3;
+        }
+        if (v == m % 2 + 1){
+            return u % 3;
+        }
+        tg = min(u, v);
+        return tg % 3;
+    }
+    else{
+        tg = min(u, v);
+        return tg % 3;
+    }
+}
+
+int main(){
+    long long n, x, y;
+    int k;
+    a[0] = 3;
+    a[1] = 1;
+    a[2] = 2;
+    a[3] = 3;
+    cin>>n;
+    cin>>k;
+    while (k--){
+        cin>>x>>y;
+        cout<<a[solve(x, y, n)]<<endl;
+    }
 }
