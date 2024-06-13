83524b0695621657d21b236fb3933fc367575273
tognoek <zgye28102003@gmail.com>
2024-06-13 23:49:44 +0700

    Ban bao nhieu tuoi -- py

--- tognoek.py
+++ tognoek.py
@@ -1,14 +1,2 @@
 n = int(input())
-if n  == 1:
-    print(1)
-elif n == 2:
-    print(2)
-elif n == 3:
-    print(6)
-elif n == 6:
-    print(60)
-else:
-    if n % 2 == 0:
-        print(n * (n-1) * (n-3))
-    else:
-        print(n * (n-1) * (n-2))
+print(2021-n)
