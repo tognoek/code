4dbce27f6f039e3d78acfbffd0e5395833649cb1
tognoek <zgye28102003@gmail.com>
2024-07-07 09:33:16 +0700

    interval

--- zgye.cpp
+++ zgye.cpp
@@ -1,65 +1,80 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-#define int long long
-#define FOR(i, a, b) for(int i = a; i <= b; i++)
-#define FOD(i, a, b) for(int i = a; i >= b; i--)
-
-const int N = 1e5 + 5;
-int n, a[N], l, r, T;
-vector<long long> myvector;
-
-void in() {
-    cin >> n >> T;
-    long long aa;
-    myvector.push_back(-10000);
-
-    for (int i = 0; i < n; i++){
-        cin >> aa;
-        myvector.push_back(aa);
-    }
-    sort(myvector.begin(), myvector.end());
+void openfile(){
+    freopen("interval.inp", "r", stdin);
+    freopen("interval.out", "w", stdout);
 }
-
-int get(int lim) {
-    int low = 1, high = n, res = 0;
-
-    while(low <= high) {
-        int mid = (low + high) / 2;
-
-        if(a[mid] <= lim) res = mid, low = mid + 1;
-        else high = mid - 1;
+int count(long long n){
+    int res = 0;
+    while (n > 0){
+        res++;
+        n = n / 10;
     }
     return res;
 }
 
-void openfile(){
-    freopen("Cuocthi.inp","r",stdin);
-    freopen("Cuocthi.out","w",stdout);
+long long dem(long long n){
+    long long r;
+    r = 1;
+    for (int i = 1; i <= n - 2; i++){
+        r = r * 10;
+    }
+    return 9 * r;
 }
 
-void process() {
-    while(T--) {
-        cin >> l >> r;
-        int left = lower_bound(myvector.begin(), myvector.end(), l) - myvector.begin() - 1;
-        // left cuối cùng < l
-        // right đầu tiền > r
-        int right = lower_bound(myvector.begin(), myvector.end(), r + 1) - myvector.begin();
-        // cout<<left<<" "<<right<<endl;
-        cout<<right - left - 1<<endl;
-        // int res = right - left;
-        // if (res == 0){
-        //     res = res;
-        // }else{
-        //     res++;
-        // }
-        // cout<<res<<endl;
+long long slove(long long n){
+    if (n < 10){
+        return n;
+    }
+    long long res = 9;
+    if (n < 100){
+        res = res + n / 10;
+        if (n / 10 > n % 10){
+            res--;
+        }
+        return res;
+    }else{
+        res = res + 9;
+    }
+
+    int l = count(n);
+
+    for (int i = 3; i < l; i++){
+        res = res + dem(i);
+    }
+    // be hon 1 chu so
+
+    long long dau;
+    dau = n;
+    while (dau >= 10){
+        dau = dau / 10;
+    }
+
+    int r = 1;
+    for (int i = 1; i <= l - 2; i++){
+        r = r * 10;
+    }
+    res = res + (dau - 1) * r;
+    long long temp = n;
+    temp = temp / 10;
+    temp = temp % r + 1;
+    res = res + temp * 1;
+
+
+    long long cuoi;
+    cuoi = n % 10;
+    if (dau > cuoi){
+        return res - 1;
+    }else{
+        return res;
     }
 }
 
-#undef int
 int main() {
     openfile();
-    in();
-    process();
+    long long L, R;
+    cin>>L>>R;
+    cout<<slove(R) - slove(L-1)<<endl;
+    // cout<<count(123);
 }
