2f4612d6863326b6ea85cc6cbc1a4e0b9107792e
tognoek <zgye28102003@gmail.com>
2024-07-27 23:32:06 +0700

    Doan con tong bang K -- zgye.cpp

--- zgye.cpp
+++ zgye.cpp
@@ -1,27 +1,35 @@
 #include <bits/stdc++.h>
 using namespace std;
 const int nMax = 100005;
-const int nMaxx2 = 1000005;
 long long a[nMax];
-long long f[nMaxx2];
 
 int main()
 {
-    // freopen("PSEQ.inp","r",stdin);
-    // freopen("PSEQ.out","w",stdout);
+    freopen("PSEQ.inp","r",stdin);
+    freopen("PSEQ.out","w",stdout);
     int n;
     long long k;
     cin>>n>>k;
+    unordered_map<long long, int> myUnorderedMap;
     for(int i = 1; i <= n; i++){
         cin>>a[i];
     }
     long long dis;
     long long s = 0;
     long long res = 0;
+    myUnorderedMap[0] = 1;
     for (int i = 1; i <= n; i++){
         s = s + a[i];
         dis = s - k;
-        res = res + 1;
+        if (myUnorderedMap.find(dis)!= myUnorderedMap.end()){
+            res = res + myUnorderedMap[dis];
+        }
+        if (myUnorderedMap.find(s) != myUnorderedMap.end()){
+            myUnorderedMap[s]++;
+        }else{
+            myUnorderedMap[s] = 1;
+        }
     }
+    cout<<res;
     return 0;
 }
