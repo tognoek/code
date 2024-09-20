26b65230ee2eb90b99a0f381a416e14cb4b922d6
tognoek <zgye28102003@gmail.com>
2024-09-20 09:50:39 +0700

    3Sum closet -- javascript

--- tognoek.js
+++ tognoek.js
@@ -3,49 +3,21 @@
  * @param {number} target
  * @return {number}
  */
-var search = function(nums, target) {
-    let left = 0;
-    let right = nums.length - 1;
-    while (left  <= right){
-        let mid = Math.floor((left + right) / 2);
-        console.log('mid:', mid);
-        if (nums[mid] === target){
-            return mid;
-        }else{
-            if (nums[left] < nums[right]){
-                if (nums[left] <= target && target < nums[mid]){
-                    right = mid - 1;
-                } else {
-                    left = mid + 1;
-                }
-            }else{
-                if (nums[mid] < nums[right]){
-                    if (nums[mid] < target){
-                        if (nums[right] < target){
-                            right = mid - 1;
-                        }else{
-                            left = mid + 1;
-                        }
-                    }else{
-                        right = mid - 1;
-                    }
-                }else{
-                    if (nums[mid] < target){
-                        left = mid + 1;
-                    }else{
-                        if (nums[left] <= target){
-                            right = mid - 1;
-                        }else{
-                            left = mid + 1;
-                        }
-                    }
+var threeSumClosest = function (nums, target) {
+    let lengthNums = nums.length;
+    let fault;
+    let closestSum;
+    for (let i = 0; i < lengthNums; i++) {
+        for (let j = i + 1; j < lengthNums; j++) {
+            for (let k = j + 1; k < lengthNums; k++) {
+                let sum = nums[i] + nums[j] + nums[k];
+                let diff = Math.abs(target - sum);
+                if (fault === undefined || diff < fault) {
+                    fault = diff;
+                    closestSum = sum;
                 }
             }
         }
-        console.log('left:', left);
-        console.log('right:', right);
     }
-    return -1;
+    return closestSum;
 };
-a = [3,5,1]
-console.log(search(a, 3))
