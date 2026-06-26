dacb3ad238e1fe6c7d3872acfae85f41c4f4041c
thong <thong@purusgames.com>
2026-06-26 17:39:29 +0700

    Helpful Maths -- rust

--- tognoek.rs
+++ tognoek.rs
@@ -1,37 +1,17 @@
+#![allow(unused_imports)]
 use std::io;
 use std::cmp::*;
 
 fn solve() {
-    let mut a = String::new();
-    io::stdin().read_line(&mut a).unwrap();
-    let mut b = String::new();
-    io::stdin().read_line(&mut b).unwrap();
-    let a = a.trim().as_bytes();
-    let b = b.trim().as_bytes();
-    let n = min(a.len(), b.len());
-    for i in 0..n {
-        let mut na = a[i] as i32;
-        let mut nb = b[i] as i32;
-        if na < 97 {
-            na = na + 32;
+    let mut input = String::new();
+    io::stdin().read_line(&mut input).unwrap();
+    let mut numbers: Vec<i32> = input.trim().split("+").map(|x| x.parse().unwrap()).collect();
+    numbers.sort();
+    for (index, value) in numbers.iter().enumerate() {
+        print!("{}", value);
+        if index != numbers.len() - 1 {
+            print!("+");
         }
-        if nb < 97 {
-            nb = nb + 32;
-        }
-        if na < nb {
-            println!("-1");
-            return;
-        } else if na > nb {
-            println!("1");
-            return;
-        }
-    }
-    if a.len() < b.len() {
-        println!("-1");
-    } else if a.len() > b.len() {
-        println!("1");
-    } else {
-        println!("0");
     }
 }
 
