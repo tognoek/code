9d7386b5b38f48b6a84fb3cba9a59855ec3d8012
thong <thong@purusgames.com>
2026-06-26 12:00:52 +0700

    Watermelon -- rust

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 **togneok** xin chào các bạn !!!
 >[!NOTE] Đọc đề để hiểu rõ yêu cầu đề nha.
-- [Đề bài](https://codeforces.com/problemset/problem/236/A)
+- [Đề bài](https://codeforces.com/problemset/problem/4/A)
 > 📝Chú ý các bài trên web nhưng được lấy dữ liệu từ file
 >  Dựa vào commit ở file README để biết file nao chứ code của **Đề bài** phía **tên**
 
--- tognoek.rs
+++ tognoek.rs
@@ -1,22 +1,19 @@
 use std::io;
 
-fn main() {
+fn solve() {
     let mut input = String::new();
-    let mut count_chart = vec![0;255];
-    io::stdin().read_line(&mut input).unwrap();
-    
-    for c in input.chars() {
-        count_chart[c as usize] = 1;
-    }
-    let mut res = 0;
-    for i in 0..255 {
-        if count_chart[i] == 1 {
-            res += 1;
-        }
-    }
-    if res % 2 == 0 {
-        println!("CHAT WITH HER!");
+    io::stdin().read_line(&mut input). unwrap();
+    let weight_watermelon = input.trim().parse::<i32>().unwrap();
+
+    if weight_watermelon % 2 == 0 && weight_watermelon > 2 {
+        println!("YES");
     } else {
-        println!("IGNORE HIM!");
+        println!("NO");
     }
+}
+
+fn main() {
+    // tognoek learning rust language
+    // today is 26/06/2026
+    solve();
 }
