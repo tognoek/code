b4817f7334eea0bab09e6bd7daf440798ffb1c6a
thong <thong@purusgames.com>
2026-06-26 16:35:46 +0700

    Domino piling -- rust

--- tognoek.rs
+++ tognoek.rs
@@ -4,17 +4,9 @@ fn solve() {
     let mut input = String::new();
     io::stdin().read_line(&mut input).unwrap();
     let nums: Vec<i32> = input.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
-    let n = nums[0];
-    let k = nums[1];
-    input.clear();
-    io::stdin().read_line(&mut input).unwrap();
-    let a: Vec<i32> = input.trim().split_whitespace().map(|x| x.parse::<i32>().unwrap()).collect();
-    let mut res = 0;
-    for i in 0..n {
-        if a[i as usize] > 0 && a[i as usize] >= a[k as usize - 1] {
-            res += 1;
-        }
-    }
+    let m = nums[0];
+    let n = nums[1];
+    let res = m * n / 2;
     println!("{}", res);
 }
 
